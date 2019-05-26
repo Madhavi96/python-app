d740ac743623d7c9ed64de2699777762f67e716f
CSEGroupProject <37502075+CSEGroupProject@users.noreply.github.com>
2019-05-26 22:33:43 +0530

    u

--- predictor.py
+++ predictor.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.svm import SVR
 
 files=['OutputDTM0.csv','OutputDTM1.csv','OutputDTM2.csv','OutputDTM3.csv','OutputDTM4.csv']
-topic_number=1
+
 
 times = ["201810", "201811", "201812", "201901", "201902", "201903", "201904"]
 monthsdict = {"201810": "October 2018", "201811": "November 2018", "201812": "Decemebr 2018", "201901": "January 2019",
@@ -11,7 +11,7 @@ monthsdict = {"201810": "October 2018", "201811": "November 2018", "201812": "De
 months=["October 2018","November 2018","December 2018","January 2019","February 2019","March 2019","April 2019"]
 monthsdictionary={"October 2018":0,"November 2018":1,"December 2018":2,"January 2019":3,"February 2019":4,"March 2019":5,"April 2019":6}
 
-def getFuturePlot(end=None,topic_number=None):
+def getFuturePlot(end,topic_number):
 
     with open(files[topic_number] , 'r') as csvfile:
         dates = []
@@ -28,7 +28,8 @@ def getFuturePlot(end=None,topic_number=None):
                 topic_chain.append(row[1])
             else:
                 pass
-
+        print(dates)
+        print(topic_chain)
         start_old=months.index(dates[0])
         end_old = months.index(dates[-1])
 
